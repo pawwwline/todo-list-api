ee4f78c4455f32090efc90f9ffd7834b238d0b0d
Полина Кузнецова <polinakuznecova@MacBook-Pro-Polina.local>
2024-10-14 22:03:56 +0500

    small fixes

--- models/task_model.go
+++ models/task_model.go
@@ -4,5 +4,5 @@ type Task struct {
 	Id          int    `json:"id"`
 	UserId      int    `json:"user_id"`
 	Title       string `json:"title"`
-	Description string `json:"decription"`
+	Description string `json:"description"`
 }
