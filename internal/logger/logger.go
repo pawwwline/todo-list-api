4500d9b58bf465dee14a5e8ed6756ab719d5ea52
Полина Кузнецова <polinakuznecova@MacBook-Pro-Polina.local>
2024-10-14 21:53:51 +0500

    refactored logger init func

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,18 +1,22 @@
 package logger
 
 import (
+	"errors"
 	"log/slog"
 	"os"
 )
 
 var Logger *slog.Logger
 
-func SetupLogger(env string) *slog.Logger {
+func SetupLogger(env string) (*slog.Logger, error) {
+	if env == "" {
+		return nil, errors.New("env is none")
+	}
 	switch env {
 	case "local", "test":
 		Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case "dev", "prod":
 		Logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
-	return Logger
+	return Logger, nil
 }
