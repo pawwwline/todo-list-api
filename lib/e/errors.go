daa9f15bab1bef9ee1e2cc94415fde159b2de450
Полина Кузнецова <polinakuznecova@MacBook-Pro-Polina.local>
2024-10-14 22:03:34 +0500

    added common errors vars

--- lib/e/errors.go
+++ lib/e/errors.go
@@ -9,6 +9,7 @@ var (
 	UniqueViolationErr    = errors.New("value already exist in db")
 	AuthorizationMissing  = errors.New("authorization header is missing")
 	InvalidCredentialsErr = errors.New("invalid email or password")
+	ItemIdNotFound        = errors.New("item id not found")
 )
 
 func WrapError(message string, err error) error {
